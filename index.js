e2abf98eaac989d15128284cb7bb5ee5169ebba2
Daniel Cousens <github@dcousens.com>
2016-09-16 13:44:33 +1000

    humanize, add colour to status code

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var bytes = require('bytes')
 var onFinished = require('on-finished')
 
 module.exports = function debugWare (debug) {
@@ -5,7 +6,15 @@ module.exports = function debugWare (debug) {
     function log (err) {
       if (err) return debug(`${req.method} ${req.url}`, err)
 
-      debug(`${req.method} ${req.url}`, res.header && res.statusCode, res.body && res.body.length || 0)
+      var status = res.statusCode
+      var size = bytes(res.body && res.body.length || 0)
+      var color = status >= 500 ? 31 // red
+        : status >= 400 ? 33 // yellow
+        : status >= 300 ? 36 // cyan
+        : status >= 200 ? 32 // green
+        : 0 // no color
+
+      debug(`${req.method} ${req.url} \x1b[${color}m${status} ${size}`)
     }
 
     onFinished(res, log)
